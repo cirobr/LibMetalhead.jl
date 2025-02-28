ddcac3b9eec28266fd4f1d848d91af9aac721efe
cirobr <ciro.rosa@alumni.usp.br>
2025-02-28 19:12:34 -0300

    change dropout

--- src/resnets.jl
+++ src/resnets.jl
@@ -20,7 +20,7 @@ function ResNet50(classes::Int=2)
             AdaptiveMeanPool((1,1)),
             Flux.flatten,
             Dense(2048, 512; init=kaiming_normal(gain=kf*√2048)), BatchNorm(512, leakyrelu),
-            Dropout(0.2),     # from htresnet50.jl
+            Dropout(0.15),     # from htresnet50.jl
             Dense(512, 64; init=kaiming_normal(gain=kf*√512)),    BatchNorm(64, leakyrelu),
             # Dropout(0.0),   # from htresnet50.jl
             Dense(64, classes; init=kaiming_normal(gain=kf*√64)), softmax   # columns are independent vectors
